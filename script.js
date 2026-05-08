efd038544e3a6a46b74abead44b7ed78dc6f6f00
Alejandra Mendez <est.mariaa.mendez@unimilitar.edu.co>
2026-05-08 12:44:38 -0500

    se realizan los calculos de las intersecciones y la logica matematica, ademas de completar el cohen-sutherlan

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const ctx = canvas.getContext("2d");
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
 
-// ======================================
+// =====================================================
 // VENTANA DE RECORTE
-// ======================================
+// =====================================================
 
 let xmin = 200;
 let ymin = 150;
@@ -15,19 +15,30 @@ let ymin = 150;
 let xmax = 600;
 let ymax = 450;
 
-// ======================================
+// =====================================================
+// CODIGOS BINARIOS
+// =====================================================
+
+const INSIDE = 0;
+
+const LEFT = 1;
+const RIGHT = 2;
+
+const BOTTOM = 4;
+const TOP = 8;
+
+// =====================================================
 // CONVERTIR EJE Y
-// ======================================
+// =====================================================
 
 function convertirY(y){
 
     return HEIGHT - y;
 }
 
-// ======================================
-// DIBUJAR GRID
-//Las líneas se trazan a partir de una función no primitiva del canvas
-// ======================================
+// =====================================================
+// GRID
+// =====================================================
 
 function dibujarGrid(){
 
@@ -56,15 +67,15 @@ function dibujarGrid(){
     }
 }
 
-// ======================================
+// =====================================================
 // FUNCION PERSONALIZADA LINEA
-// ======================================
+// =====================================================
 
 function dibujarLinea(x1, y1, x2, y2, color){
 
     ctx.strokeStyle = color;
 
-    ctx.lineWidth = 3;
+    ctx.lineWidth = 4;
 
     ctx.beginPath();
 
@@ -75,9 +86,9 @@ function dibujarLinea(x1, y1, x2, y2, color){
     ctx.stroke();
 }
 
-// ======================================
-// FUNCION VIEWPORT
-// ======================================
+// =====================================================
+// VIEWPORT
+// =====================================================
 
 function dibujarViewport(){
 
@@ -93,9 +104,166 @@ function dibujarViewport(){
     );
 }
 
-// ======================================
+// =====================================================
+// OBTENER CODIGO
+// =====================================================
+
+function obtenerCodigo(x, y){
+
+    let codigo = INSIDE;
+
+    // izquierda
+    if(x < xmin){
+
+        codigo |= LEFT;
+    }
+
+    // derecha
+    else if(x > xmax){
+
+        codigo |= RIGHT;
+    }
+
+    // abajo
+    if(y < ymin){
+
+        codigo |= BOTTOM;
+    }
+
+    // arriba
+    else if(y > ymax){
+
+        codigo |= TOP;
+    }
+
+    return codigo;
+}
+
+// =====================================================
+// ALGORITMO COHEN-SUTHERLAND
+// =====================================================
+
+function cohenSutherland(x1, y1, x2, y2){
+
+    let codigo1 = obtenerCodigo(x1, y1);
+
+    let codigo2 = obtenerCodigo(x2, y2);
+
+    let aceptar = false;
+
+    while(true){
+
+        // totalmente dentro
+
+        if((codigo1 | codigo2) === 0){
+
+            aceptar = true;
+
+            break;
+        }
+
+        // totalmente fuera
+
+        else if((codigo1 & codigo2) !== 0){
+
+            break;
+        }
+
+        // parcialmente dentro
+
+        else{
+
+            let codigoFuera;
+
+            let x, y;
+
+            if(codigo1 !== 0){
+
+                codigoFuera = codigo1;
+            }
+
+            else{
+
+                codigoFuera = codigo2;
+            }
+
+            // arriba
+
+            if(codigoFuera & TOP){
+
+                x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1);
+
+                y = ymax;
+            }
+
+            // abajo
+
+            else if(codigoFuera & BOTTOM){
+
+                x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1);
+
+                y = ymin;
+            }
+
+            // derecha
+
+            else if(codigoFuera & RIGHT){
+
+                y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1);
+
+                x = xmax;
+            }
+
+            // izquierda
+
+            else if(codigoFuera & LEFT){
+
+                y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1);
+
+                x = xmin;
+            }
+
+            // actualizar punto 1
+
+            if(codigoFuera === codigo1){
+
+                x1 = x;
+                y1 = y;
+
+                codigo1 = obtenerCodigo(x1, y1);
+            }
+
+            // actualizar punto 2
+
+            else{
+
+                x2 = x;
+                y2 = y;
+
+                codigo2 = obtenerCodigo(x2, y2);
+            }
+        }
+    }
+
+    // retornar linea recortada
+
+    if(aceptar){
+
+        return {
+
+            x1,
+            y1,
+            x2,
+            y2
+        };
+    }
+
+    return null;
+}
+
+// =====================================================
 // DIBUJAR ESCENA
-// ======================================
+// =====================================================
 
 function dibujar(){
 
@@ -105,15 +273,41 @@ function dibujar(){
 
     dibujarViewport();
 
-    // linea de prueba
+    // linea original
+
+    let x1 = 100;
+    let y1 = 100;
+
+    let x2 = 700;
+    let y2 = 500;
 
     dibujarLinea(
-        100,
-        100,
-        700,
-        500,
-        "red"
+        x1,
+        y1,
+        x2,
+        y2,
+        "gray"
     );
+
+    // linea recortada
+
+    let resultado = cohenSutherland(
+        x1,
+        y1,
+        x2,
+        y2
+    );
+
+    if(resultado){
+
+        dibujarLinea(
+            resultado.x1,
+            resultado.y1,
+            resultado.x2,
+            resultado.y2,
+            "red"
+        );
+    }
 }
 
 dibujar();
